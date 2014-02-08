fff1076627d01081e5c23ae384aaec748299c57e
Peter Wilmott <p@p8952.info>
2014-02-08 12:13:04 +0000

    move player on keyboard input not mouse clicks

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -3,34 +3,48 @@ class GameWindow < Gosu::Window
 	def initialize
 		super(720, 720, false)
 		self.caption = "Gosu"
-		@player = Player.new(self)
 		@background = Gosu::Image.new(self, 'media/background.png', false)
-		@flag = Gosu::Image.new(self, 'media/flag.png', false)
-		@x_click = 320
-		@y_click = 240
+		@player = Player.new(self)
+		@x_target = @player.get_x
+		@y_target = @player.get_y
 	end
 
 	def button_down(id)
-		
 		if id == Gosu::KbEscape
 			close
 		end
-
-		if id == Gosu::MsLeft
-			@x_click = (((self.mouse_x / 24).round) * 24)
-			@y_click = (((self.mouse_y / 24).round) * 24)
-		end
-
 	end
 
 	def update
-		@player.move(@x_click, @y_click)
+		if button_down?(Gosu::KbRight)
+			if @player.get_x == @x_target
+				@x_target += 24
+			end
+		end
+
+		if button_down?(Gosu::KbLeft)
+			if @player.get_x == @x_target
+				@x_target += -24
+			end
+		end
+		
+		if button_down?(Gosu::KbUp)
+			if @player.get_y == @y_target
+				@y_target += -24
+			end
+		end
+		
+		if button_down?(Gosu::KbDown)
+			if @player.get_y == @y_target
+				@y_target += 24
+			end
+		end
+
+		@player.move(@x_target, @y_target)
 	end
 
 	def draw
 		@background.draw(0, 0, 0)
-		@flag.draw(@x_click, @y_click, 1)
-		@flag.draw((((self.mouse_x / 24).round) * 24), (((self.mouse_y / 24).round) * 24), 1)
 		@player.draw
 	end
 
--- lib/player.rb
+++ lib/player.rb
@@ -2,9 +2,17 @@ class Player
 
 	def initialize(game_window)
 		@image = Gosu::Image.new(game_window, 'media/player.png', false)
-		@x_pos = 320
-		@y_pos = 240
+		@x_pos = 0
+		@y_pos = 0
 		@speed = 1
+	end
+
+	def get_x
+		return @x_pos
+	end
+
+	def get_y
+		return @y_pos
 	end
 
 	def move(x_target, y_target)
