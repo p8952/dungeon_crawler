dcc4f9266f6c211a6b51d250a863618bf8740610
Peter Wilmott <p@p8952.info>
2014-04-12 13:44:29 +0100

    move player movement logic from the main loop to the Player class

--- lib/game_window.rb
+++ lib/game_window.rb
@@ -1,5 +1,7 @@
 class GameWindow < Gosu::Window
 
+	attr_accessor :cave
+
 	def initialize
 		super(720, 720, false)
 		self.caption = "Gosu"
@@ -9,59 +11,30 @@ class GameWindow < Gosu::Window
 		
 		@cave = Cave.new(720, 720)
 		@player = Player.new()
-		@x_target = @player.x_pos
-		@y_target = @player.y_pos
-	end
-
-	def button_down(id)
-		if id == Gosu::KbEscape
-			close
-		end
 	end
 
 	def update
-		if button_down?(Gosu::KbRight)
-			if @player.x_pos == @x_target
-				if path_clear(@x_target + 24, @y_target)
-					@x_target += 24
-				end
-			end
+		if button_down?(Gosu::KbEscape)
+			close
 		end
 
-		if button_down?(Gosu::KbLeft)
-			if @player.x_pos == @x_target
-				if path_clear(@x_target - 24, @y_target)
-					@x_target += -24
-				end
-			end
-		end
-		
 		if button_down?(Gosu::KbUp)
-			if @player.y_pos == @y_target
-				if path_clear(@x_target, @y_target - 24)
-					@y_target += -24
-				end
-			end
+			@player.move(:up)
 		end
 		
 		if button_down?(Gosu::KbDown)
-			if @player.y_pos == @y_target
-				if path_clear(@x_target, @y_target + 24)
-					@y_target += 24
-				end
-			end
+			@player.move(:down)
 		end
 
-		@player.move(@x_target, @y_target)
-	end
-
-	def path_clear(x_target, y_target)
-		if @cave.cells["#{x_target} #{y_target}"].nil? or
-		   @cave.cells["#{x_target} #{y_target}"].state == 'wall'
-		   	return false
-		else
-			return true
+		if button_down?(Gosu::KbRight)
+			@player.move(:right)
 		end
+
+		if button_down?(Gosu::KbLeft)
+			@player.move(:left)
+		end
+
+		$game_objects.each { |obj| obj.update }
 	end
 
 	def draw
--- lib/player.rb
+++ lib/player.rb
@@ -1,30 +1,77 @@
 class Player
 
-	attr_accessor :x_pos, :y_pos
+	attr_accessor :x_pos, :y_pos, :x_target, :y_target
 
 	def initialize()
 		@sprite = [$spritesheet[6], $spritesheet[7]]
 		@x_pos = 24
 		@y_pos = 24
+		@x_target = 24
+		@y_target = 24
 		@speed = 1
 		$game_objects << self
 	end
 
-	def move(x_target, y_target)
+	def path_clear(x_target, y_target)
+		if $game_window.cave.cells["#{x_target} #{y_target}"].nil? or
+		   $game_window.cave.cells["#{x_target} #{y_target}"].state == 'wall'
+		   	return false
+		else
+			return true
+		end
+	end
 
-		if @x_pos < x_target
+	def move(button)
+
+		if button == :up
+			if @y_pos == @y_target
+				if path_clear(@x_target, @y_target - 24)
+					@y_target += -24
+				end
+			end
+		end
+		
+		if button == :down
+			if @y_pos == @y_target
+				if path_clear(@x_target, @y_target + 24)
+					@y_target += 24
+				end
+			end
+		end
+
+		if button == :right
+			if @x_pos == @x_target
+				if path_clear(@x_target + 24, @y_target)
+					@x_target += 24
+				end
+			end
+		end
+
+		if button == :left
+			if @x_pos == @x_target
+				if path_clear(@x_target - 24, @y_target)
+					@x_target += -24
+				end
+			end
+		end
+
+	end
+
+	def update()
+
+		if @x_pos < @x_target
 			@x_pos += @speed
 		end
 		
-		if @x_pos > x_target
+		if @x_pos > @x_target
 			@x_pos += -@speed
 		end
 		
-		if @y_pos < y_target
+		if @y_pos < @y_target
 			@y_pos += @speed
 		end
 
-		if @y_pos > y_target
+		if @y_pos > @y_target
 			@y_pos += -@speed
 		end
 			
