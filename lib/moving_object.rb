4b606a34c9661e7f87af1073789965bb65790f07
Peter Wilmott <p@p8952.info>
2014-06-05 18:07:41 +0100

    put movement logic into the moving object class

--- lib/moving_object.rb
+++ lib/moving_object.rb
@@ -9,6 +9,51 @@ class MovingObject < StaticObject
 		@speed = 1
 	end
 
+	def path_clear(x_target, y_target)
+		if $game_window.cave.cells["#{x_target} #{y_target}"].nil? or
+		   $game_window.cave.cells["#{x_target} #{y_target}"].state == :blocking
+		   	return false
+		else
+			return true
+		end
+	end
+
+	def action(action)
+
+		if action == :up
+			if @y_pos == @y_target
+				if path_clear(@x_target, @y_target - 24)
+					@y_target += -24
+				end
+			end
+		end
+		
+		if action == :down
+			if @y_pos == @y_target
+				if path_clear(@x_target, @y_target + 24)
+					@y_target += 24
+				end
+			end
+		end
+
+		if action == :right
+			if @x_pos == @x_target
+				if path_clear(@x_target + 24, @y_target)
+					@x_target += 24
+				end
+			end
+		end
+
+		if action == :left
+			if @x_pos == @x_target
+				if path_clear(@x_target - 24, @y_target)
+					@x_target += -24
+				end
+			end
+		end
+
+	end
+
 	def update()
 		
 		if @y_pos > @y_target
--- lib/player.rb
+++ lib/player.rb
@@ -5,51 +5,6 @@ class Player < MovingObject
 		@sprite = [$spritesheet[8], $spritesheet[9]]
 	end
 
-	def path_clear(x_target, y_target)
-		if $game_window.cave.cells["#{x_target} #{y_target}"].nil? or
-		   $game_window.cave.cells["#{x_target} #{y_target}"].state == :blocking
-		   	return false
-		else
-			return true
-		end
-	end
-
-	def action(action)
-
-		if action == :up
-			if @y_pos == @y_target
-				if path_clear(@x_target, @y_target - 24)
-					@y_target += -24
-				end
-			end
-		end
-		
-		if action == :down
-			if @y_pos == @y_target
-				if path_clear(@x_target, @y_target + 24)
-					@y_target += 24
-				end
-			end
-		end
-
-		if action == :right
-			if @x_pos == @x_target
-				if path_clear(@x_target + 24, @y_target)
-					@x_target += 24
-				end
-			end
-		end
-
-		if action == :left
-			if @x_pos == @x_target
-				if path_clear(@x_target - 24, @y_target)
-					@x_target += -24
-				end
-			end
-		end
-
-	end
-
 	def draw()
 		@sprite[Gosu::milliseconds / 500 % 2].draw(@x_pos, @y_pos, 99)
 	end
